ce90498f112a31c5183f262a9bd4dbbe63220be2
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-07 08:58:33 -0300

    Update 07.py

--- day07/07.py
+++ day07/07.py
@@ -8,17 +8,9 @@ input = '''190: 10 19
 21037: 9 7 18 13
 292: 11 6 16 20'''.splitlines()
 
-def can_combine(v, os, acc):
-    if not os:
-        return v == acc
-    return (can_combine(v, os[1:], acc + os[0]) or
-            can_combine(v, os[1:], acc * os[0]) or
-            can_combine(v, os[1:], concat(acc, os[0])))
-
 from math import log10
-def concat(big, small):
-    return big * 10 ** (int(log10(small)) + 1) + small
+# Check the commit history for a more readable version
+def can_combine(v, os, acc):
+    return v == acc if not os else (can_combine(v, os[1:], acc + os[0]) or can_combine(v, os[1:], acc * os[0]) or can_combine(v, os[1:], acc * 10 ** (int(log10(os[0])) + 1) + os[0]))
 
-equations = {int(test_value): [int(o) for o in operands.split(' ')] for test_value, operands in (eq.split(': ') for eq in input)}
-
-print(sum((test_value for test_value, operands in equations.items() if can_combine(test_value, operands, 0))))
+print(sum((test_value for test_value, operands in {int(test_value): [int(o) for o in operands.split(' ')] for test_value, operands in (eq.split(': ') for eq in input)}.items() if can_combine(test_value, operands, 0))))

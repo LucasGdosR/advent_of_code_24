424e236b00f913995cd59b2654b12d9653bc6f2f
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-10 17:28:52 -0300

    Update 4-1.c

--- day04/4-1.c
+++ day04/4-1.c
@@ -4,20 +4,18 @@
 #define GRID_SIZE_PLUS_SENTINELS GRID_SIZE + 2
 
 void read_input();
-void add_sentinels();
-int find_MAS(int x, int y);
+int find_cross(int x, int y);
 
 char input[GRID_SIZE_PLUS_SENTINELS][GRID_SIZE_PLUS_SENTINELS];
 
 void main(int argc, char const *argv[]) {
     read_input();
-    add_sentinels();
 
     int count = 0;
     for (int i = 1; i <= GRID_SIZE; i++) {
         for (int j = 1; j <= GRID_SIZE; j++) {
             if (input[i][j] == 'X') {
-                count += find_MAS(i, j);
+                count += find_cross(i, j);
             }
         }    
     }
@@ -30,7 +28,7 @@ const int DIRECTIONS[8][2] = {
     {-1,  1}, {0,  1}, {1,  1}
 };
 
-int find_MAS(int x, int y) {
+int find_cross(int x, int y) {
     int count = 0;
     for (int i = 0; i < 8; i++) {
         int dx = DIRECTIONS[i][0], dy = DIRECTIONS[i][1];
@@ -48,13 +46,4 @@ void read_input() {
         fgets(input[i + 1] + 1, GRID_SIZE + 2, fptr);
     }
     fclose(fptr);
-}
-
-void add_sentinels() {
-    for (int i = 0; i < GRID_SIZE_PLUS_SENTINELS; i++) {
-        input[0][i] = 0;
-        input[GRID_SIZE_PLUS_SENTINELS - 1][i] = 0;
-        input[i][0] = 0;
-        input[i][GRID_SIZE_PLUS_SENTINELS - 1] = 0;
-    } 
 }

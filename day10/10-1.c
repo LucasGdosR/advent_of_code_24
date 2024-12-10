2cdcba052d81be152f9721a0742bc600acb810bf
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-10 19:53:56 -0300

    Update 10-1.c

--- day10/10-1.c
+++ day10/10-1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 #define TRAILHEAD '0'
 #define HILLTOP '9'
@@ -9,7 +9,6 @@
 
 void hike_uphill(__uint16_t p, char c, __uint16_t starting_point);
 void read_input();
-void add_sentinels();
 
 char grid[GRID_SIZE_PLUS_SENTINELS][GRID_SIZE_PLUS_SENTINELS];
 __uint16_t hilltop_reached[GRID_SIZE_PLUS_SENTINELS][GRID_SIZE_PLUS_SENTINELS];
@@ -17,14 +16,13 @@ __uint16_t score;
 
 void main(int argc, char **argv) {
     read_input();
-    memset(hilltop_reached, 0, sizeof(hilltop_reached));
     
     for (__uint16_t i = 1; i < GRID_SIZE_PLUS_SENTINELS - 1; i++) {
         for (__uint8_t j = 1; j < GRID_SIZE_PLUS_SENTINELS - 1; j++) {
             char c = grid[i][j];
             if (c == TRAILHEAD) {
                 __uint16_t starting_point = (i << 8) | j;
-                hike_uphill(starting_point, c, starting_point);
+                hike_uphill(starting_point, TRAILHEAD, starting_point);
             }
         }
     }
@@ -32,10 +30,10 @@ void main(int argc, char **argv) {
 }
 
 void hike_uphill(__uint16_t p, char c, __uint16_t starting_point) {
-    __uint8_t i = starting_point >> 8, j = starting_point & 0xFF;
+    __uint8_t i = p >> 8, j = p & 0xFF;
     if (c == HILLTOP) {
         score += hilltop_reached[i][j] != starting_point;
-        hilltop_reached[i][j];
+        hilltop_reached[i][j] = starting_point;
         return;
     }
 
@@ -55,13 +53,4 @@ void read_input() {
         fgets(grid[i + 1] + 1, GRID_SIZE + 2, fptr);
     }
     fclose(fptr);
-}
-
-void add_sentinels() {
-    for (int i = 0; i < GRID_SIZE_PLUS_SENTINELS; i++) {
-        grid[0][i] = 0;
-        grid[GRID_SIZE_PLUS_SENTINELS - 1][i] = 0;
-        grid[i][0] = 0;
-        grid[i][GRID_SIZE_PLUS_SENTINELS - 1] = 0;
-    } 
 }

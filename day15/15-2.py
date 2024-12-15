03def2c2edd49af784f266cc99ed00204dad236d
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-15 11:52:46 -0300

    Update 15-2.py

--- day15/15-2.py
+++ day15/15-2.py
@@ -1,34 +1,42 @@
+old_warehouse = '''##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########'''.splitlines()
+
+moves = '''<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^'''
+
+from collections import deque
+
 new_warehouse = [[] for _ in range(len(old_warehouse))]
 for i, row in enumerate(old_warehouse):
     for j, c in enumerate(row):
-        if c == '#':
-            new_warehouse[i].append('#')
-            new_warehouse[i].append('#')
-        elif c == '.':
-            new_warehouse[i].append('.')
-            new_warehouse[i].append('.')
-        elif c == 'O':
+        if c == 'O':
             new_warehouse[i].append('[')
             new_warehouse[i].append(']')
-        else:
+        elif c == '@':
             robot = [i, 2 * j]
             new_warehouse[i].append('@')
             new_warehouse[i].append('.')
-            
-def get_directions(m):
-    if m == '<':
-        return 0, -1
-    elif m == '>':
-        return 0, 1
-    elif m == '^':
-        return -1, 0
-    elif m == 'v':
-        return 1, 0
-    else:
-        return 0, 0
+        else:
+            new_warehouse[i].append(c)
+            new_warehouse[i].append(c)
 
 def try_to_move(m):
-    global new_warehouse
     di, dj = get_directions(m)
     
     dst_i = robot[0] + di
@@ -45,9 +53,7 @@ def try_to_move(m):
             try_to_move_large_box_horizontally(dj)
         elif can_move_vertically(dst_i, dst_j, di):
             offset = dst == ']'
-            temp_warehouse = [[c for c in s] for s in new_warehouse]
-            move_large_box_vertically(dst_i, dst_j - offset, di, temp_warehouse)
-            new_warehouse = temp_warehouse
+            move_large_box_vertically(dst_i, dst_j - offset, di)
             new_warehouse[dst_i][dst_j] = '@'
             new_warehouse[dst_i][(dst_j + 1) if dst == '[' else (dst_j - 1)] = '.'
             new_warehouse[robot[0]][robot[1]] = '.'
@@ -86,39 +92,52 @@ def can_move_vertically(i, j, di):
     i += di
     return can_move_vertically(i, j - offset, di) and can_move_vertically(i, j - offset + 1, di)
 
-# .... -> .[].
-# .[]. -> [][]
-# [][] -> .[].
-# .[]. -> ..@.
-# ..@. -> ....
-def move_large_box_vertically(i, left_j, di, temp_warehouse):
-    # i and left_j are the coordinates of this box
-    # Put this box on the next line
-    next_i = i + di
-    right_j = left_j + 1
-    temp_warehouse[next_i][left_j] = '['
-    temp_warehouse[next_i][right_j] = ']'
-    
-    # If the next line had boxes, move them and clean them up
-    old_left, old_right = new_warehouse[next_i][left_j], new_warehouse[next_i][right_j]
-    
-    if old_left == '[':
-        move_large_box_vertically(next_i, left_j, di, temp_warehouse)
+def move_large_box_vertically(i, left_j, di):
+    # Move levels of boxes, from deepest to the original box.
+    acc = [(i, left_j)]
+    level = deque([(i, left_j)])
+
+    while level:
+        curr_i, curr_j = level.popleft()
+        next_i = curr_i + di
+        next_left = new_warehouse[next_i][curr_j]
+        if next_left == '[':
+            box = (next_i, curr_j)
+            acc.append(box)
+            level.append(box)
+        else:
+            next_right = new_warehouse[next_i][curr_j + 1]
+            if next_left == ']':
+                box = (next_i, curr_j - 1)
+                acc.append(box)
+                level.append(box)
+            if next_right == '[':
+                box = (next_i, curr_j + 1)
+                acc.append(box)
+                level.append(box)
+
+    while acc:
+        # Put [] at the destination. Put .. at the origin.
+        curr_i, curr_j = acc.pop()
+        new_warehouse[curr_i][curr_j] = '.'
+        new_warehouse[curr_i][curr_j + 1] = '.'
+        new_warehouse[curr_i + di][curr_j] = '['
+        new_warehouse[curr_i + di][curr_j + 1] = ']'
+
+def get_directions(m):
+    if m == '<':
+        return 0, -1
+    elif m == '>':
+        return 0, 1
+    elif m == '^':
+        return -1, 0
+    elif m == 'v':
+        return 1, 0
     else:
-        if old_left == ']':
-            move_large_box_vertically(next_i, left_j - 1, di, temp_warehouse)
-            temp_warehouse[next_i][left_j - 1] = '.'
-        if old_right == '[':
-            move_large_box_vertically(next_i, right_j, di, temp_warehouse)
-            temp_warehouse[next_i][right_j + 1] = '.'
+        return 0, 0
 
 for m in moves:
     try_to_move(m)
-
-for row in new_warehouse:
-    for c in row:
-        print(c, end='')
-    print()
 
 gps = 0
 for i, row in enumerate(new_warehouse):
@@ -126,4 +145,4 @@ for i, row in enumerate(new_warehouse):
         if c == '[':
             gps += 100 * i + j
   
-print(gps) # 1404922 too low
+print(gps)

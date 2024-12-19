7f400b7db3e2f876a65e3903ea9d8135a12acd84
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-19 08:03:02 -0300

    Update 19-1.py

--- day19/19-1.py
+++ day19/19-1.py
@@ -1,4 +1,4 @@
-available_towels = set('''r, wr, b, g, bwu, rb, gb, br'''.split(', '))
+available_towels = '''r, wr, b, g, bwu, rb, gb, br'''.split(', ')
 
 target_patterns = '''brwrr
 bggr
@@ -9,22 +9,10 @@ bwurrg
 brgr
 bbrgwb'''.splitlines()
 
-memo = {}
+memo = {'': True}
 
 def can_form(pattern):
-    if pattern in memo:
-        return memo[pattern]
-    if pattern in available_towels:
-        return True
-    if len(pattern) == 1:
-        return False
-    
-    for i in range(1, len(pattern)):
-        if can_form(pattern[i:]) and can_form(pattern[:i]):
-            memo[pattern] = True
-            return True
-    
-    memo[pattern] = False
-    return False
-
-print(sum((can_form(pattern) for pattern in target_patterns)))
+    if pattern not in memo:
+        memo[pattern] = any(can_form(pattern[len(towel):]) for towel in available_towels if pattern.startswith(towel))
+    return memo[pattern]
+print(sum(can_form(pattern) for pattern in target_patterns))

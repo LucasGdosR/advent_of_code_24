0290a3c07145d5f17867ec2a93ce5dffb2a61b10
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-20 07:42:59 -0300

    Create 20-1.py

---
+++ day20/20-1.py
@@ -0,0 +1,27 @@
+racetrack = '''your input here'''.splitlines()
+
+DIRECTIONS =            ((-1, 0), (1, 0), (0, 1), (0, -1))
+TWO_PS_CHEATING_JUMPS = ((-2, 0), (2, 0), (0, 2), (0, -2))
+
+path = {} # key: (i, j); val: steps from start to (i, j)
+
+# Problem is too deep for recursive implementation
+def iterative_dfs(i, j, steps):
+    stack = [(i, j, steps)]
+    while stack:
+        i, j, steps = stack.pop()
+        for di, dj in DIRECTIONS:
+            if racetrack[i+di][j+dj] != '#' and (i+di, j+dj) not in path:
+                path[(i+di, j+dj)] = steps + 1
+                stack.append((i+di, j+dj, steps+1))
+
+for i, row in enumerate(racetrack):
+    for j, c in enumerate(row):
+        if c == 'S':
+            path[(i, j)] = 0
+            iterative_dfs(i, j, 0)
+
+def find_cheats(i, j, steps):
+    return sum(1 for di, dj in TWO_PS_CHEATING_JUMPS if 0 <= i+di < len(racetrack) and 0 <= j+dj < len(racetrack[0]) and (i+di, j+dj) in path and path[(i+di, j+dj)] >= steps + 102)
+    
+print(sum(find_cheats(i, j, steps) for (i, j), steps in path.items()))

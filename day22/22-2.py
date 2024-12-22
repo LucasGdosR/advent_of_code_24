4288832405c7276a13b2028f9022571b42db3140
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-22 07:08:27 -0300

    Create 22-2.py

---
+++ day22/22-2.py
@@ -0,0 +1,30 @@
+input = '''1
+2
+3
+2024'''.splitlines()
+
+def random(seed):
+    seed = (seed ^ (seed << 6)) % 16777216
+    seed = (seed ^ (seed >> 5)) % 16777216
+    return (seed ^ (seed << 11)) % 16777216
+
+from collections import defaultdict
+
+profits = defaultdict(lambda: 0)
+
+for seed in input:
+    seed = int(seed)
+    price_changes = []
+    seen_sequences = set()
+    for _ in range(2000):
+        prev_price = seed % 10
+        seed = random(seed)
+        curr_price = seed % 10
+        price_changes.append(curr_price - prev_price)
+        if len(price_changes) >= 4:
+            seq = tuple(price_changes[-4:])
+            if seq not in seen_sequences:
+                seen_sequences.add(seq)
+                profits[seq] += curr_price
+
+print(max(profits.items(), key=lambda i: i[1]))

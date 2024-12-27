b47a211be2456346c321fce8edc1896879b3c332
Lucas Gribel dos Reis <109366863+LucasGdosR@users.noreply.github.com>
2024-12-27 05:57:56 -0300

    Update 24-1.c

--- day24/24-1.c
+++ day24/24-1.c
@@ -6,20 +6,17 @@
 #define LINE_LENGTH 20
 #define LETTERS 26
 
+#define CHAR_TO_I(i_or_o) ((r.i_or_o.b - '0') * 10 + (r.i_or_o.c - '0'))
+#define TRI_TO_I(i_or_o) [r.i_or_o.a - 'a'][r.i_or_o.b - 'a'][r.i_or_o.c - 'a']
+
 typedef unsigned char u8;
+typedef signed   char i8;
 
 struct state {
-    u8 x[STATE_BITS];
-    u8 y[STATE_BITS];
-    u8 z[STATE_BITS + 1];
-    signed char other[LETTERS][LETTERS][LETTERS];
-};
-
-enum Op {
-    DONE,
-    AND,
-    OR,
-    XOR
+    i8 x[STATE_BITS];
+    i8 y[STATE_BITS];
+    i8 z[STATE_BITS + 1];
+    i8 other[LETTERS][LETTERS][LETTERS];
 };
 
 struct trigram {
@@ -28,12 +25,83 @@ struct trigram {
     char c;
 };
 
+typedef i8 (*op_func)(i8, i8);
+
 struct rule {
     struct trigram i1;
     struct trigram i2;
     struct trigram o;
-    enum Op op;
+    op_func func;
 };
+
+u8 rules_mapping[LETTERS][LETTERS][LETTERS];
+
+i8 and(i8 s1, i8 s2) { return s1 & s2; }
+i8 or (i8 s1, i8 s2) { return s1 | s2; }
+i8 xor(i8 s1, i8 s2) { return s1 ^ s2; }
+
+char dfs(u8 i, struct rule rules[RULES_NUM], struct state *state);
+void read_input(struct state *s, struct rule rules[RULES_NUM]);
+
+void main(int argc, char const *argv[])
+{
+    struct rule rules[RULES_NUM];
+    struct state state;
+    memset(state.other, -1, sizeof(state.other));
+    read_input(&state, rules);
+
+    long total = 0;
+    for (u8 i = 0; i < RULES_NUM; i++)
+    {
+        struct rule r = rules[i];
+        if (r.o.a == 'z') {
+            total |= (long)dfs(i, rules, &state) << CHAR_TO_I(o);
+        }
+    }
+    
+    printf("Total: %ld\n", total);
+}
+
+char dfs(u8 i, struct rule rules[RULES_NUM], struct state *state) {
+    struct rule r = rules[i];
+
+    i8 s1, s2;
+    if (r.i1.a == 'x')
+    {
+        s1 = state->x[CHAR_TO_I(i1)];
+        s2 = state->y[CHAR_TO_I(i2)];
+    }
+    else if (r.i1.a == 'y')
+    {
+        s1 = state->y[CHAR_TO_I(i1)];
+        s2 = state->x[CHAR_TO_I(i2)];
+    }
+    else
+    {
+        s1 = state->other TRI_TO_I(i1);
+        s2 = state->other TRI_TO_I(i2);
+        if (s1 == -1)
+        {
+            s1 = dfs(rules_mapping TRI_TO_I(i1), rules, state);
+        }
+        if (s2 == -1)
+        {
+            s2 = dfs(rules_mapping TRI_TO_I(i2), rules, state);
+        }
+    }
+    i8 s = r.func(s1, s2);
+    
+    if (r.o.a == 'z')
+    {
+        state->z[CHAR_TO_I(o)] = s;
+    }
+    else
+    {
+        state->other TRI_TO_I(o) = s;
+    }
+
+    return s;
+}
 
 void read_input(struct state *s, struct rule rules[RULES_NUM]) {
     FILE *f = fopen("24-input-state", "r");
@@ -54,102 +122,19 @@ void read_input(struct state *s, struct rule rules[RULES_NUM]) {
         fgets(buffer, LINE_LENGTH, f);
         struct trigram i1 = *((struct trigram*)strtok(buffer, " "));
         char *op = strtok(NULL, " ");
-        enum Op Op;
+        op_func func;
         switch (op[0])
         {
-        case 'A': { Op = AND; break; }
-        case 'O': { Op = OR ; break; }
-        case 'X': { Op = XOR; break; }
+        case 'A': { func = and; break; }
+        case 'O': { func = or ; break; }
+        case 'X': { func = xor; break; }
         }
         struct trigram i2 = *((struct trigram*)strtok(NULL, " "));
         strtok(NULL, " "); // Skip arrow
         struct trigram o = *((struct trigram*)strtok(NULL, " "));
 
-        rules[i] = (struct rule) { .i1=i1, .i2=i2, .op=Op, .o=o };
+        rules[i] = (struct rule) { .i1=i1, .i2=i2, .func=func, .o=o };
+        rules_mapping[o.a - 'a'][o.b - 'a'][o.c - 'a'] = i;
     }
     fclose(f);
-}
-
-u8 apply(enum Op op, u8 s1, u8 s2) {
-    u8 result;
-    switch (op)
-    {
-    case AND: { result = s1 & s2; break; }
-    case OR:  { result = s1 | s2; break; }
-    case XOR: { result = s1 ^ s2; break; }
-    }
-    return result;
-}
-
-void main(int argc, char const *argv[])
-{
-    struct rule rules[RULES_NUM];
-    struct state state;
-    memset(state.other, -1, sizeof(state.other));
-    read_input(&state, rules);
-
-    u8 rules_to_apply;
-    do {
-        rules_to_apply = 0;
-        for (u8 i = 0; i < RULES_NUM; i++)
-        {
-            struct rule r = rules[i];
-            if (r.op)
-            {
-                #define APPLY_RULE(x, y) \
-                u8 i1 = (r.i1.b - '0') * 10 + (r.i1.c - '0'); \
-                u8 i2 = (r.i2.b - '0') * 10 + (r.i2.c - '0'); \
-                u8 result = apply(r.op, state.x[i1], state.y[i2]); \
-                if (r.o.a == 'z') \
-                { \
-                    u8 zi = (r.o.b - '0') * 10 + (r.o.c - '0'); \
-                    state.z[zi] = result; \
-                } \
-                else \
-                { \
-                    state.other[r.o.a - 'a'][r.o.b - 'a'][r.o.c - 'a'] = result; \
-                } \
-                r.op = DONE;
-
-                if (r.i1.a == 'x')
-                {
-                    APPLY_RULE(x, y)
-                }
-                else if (r.i1.a == 'y')
-                {
-                    APPLY_RULE(y, x)
-                }
-                else
-                {
-                    char s1 = state.other[r.i1.a - 'a'][r.i1.b - 'a'][r.i1.c - 'a'];
-                    char s2 = state.other[r.i2.a - 'a'][r.i2.b - 'a'][r.i2.c - 'a'];
-                    if (s1 != -1 && s2 != -1)
-                    {
-                        u8 s = apply(r.op, s1, s2);
-                        if (r.o.a == 'z')
-                        {
-                            u8 zi = (r.o.b - '0') * 10 + (r.o.c - '0');
-                            state.z[zi] = s;
-                        }
-                        else
-                        {
-                            state.other[r.o.a - 'a'][r.o.b - 'a'][r.o.c - 'a'] = s;
-                        }
-                        rules[i].op = DONE;
-                    }
-                    else
-                    {
-                        rules_to_apply = 1;
-                    }
-                }
-            }
-        }
-    } while (rules_to_apply);
-
-    long total = 0;
-    for (u8 i = 0; i < STATE_BITS + 1; i++)
-    {
-        total |= (long)state.z[i] << i;
-    }
-    printf("Total: %ld\n", total);
 }
